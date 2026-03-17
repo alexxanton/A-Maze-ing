04a8e415f0ecc2fdd7eac71cc2b59f840e6157ca
alexxanton <alexantonaranda@gmail.com>
2026-03-17 01:47:15 +0100

    implemented prim algorithm and improved maze rendering

--- draw_maze.py
+++ draw_maze.py
@@ -2,18 +2,22 @@ from generator import Maze, Direction
 
 
 #print(wall * (maze.width * 2 + 1))
+#print(hex(maze.grid[i][j])[2:], end="")
+
 def draw_maze(maze: Maze) -> None:
-    wall = "██"
-    empty = "  "
-    maze.grid[5][5] = 0
     for i in range(len(maze.grid)):
         for j in range(len(maze.grid[i])):
             cell = maze.grid[i][j]
-            print(wall if cell & Direction.NORTH else empty, end="")
-            print(wall, end="")
-            #print(hex(maze.grid[i][j])[2:], end="")
+            print("+", end="")
+            if cell & Direction.NORTH:
+                print("--", end="")
+            else:
+                print("  ", end="")
         print()
         for j in range(len(maze.grid[i])):
-            print(empty, end="")
-            print(wall if cell & Direction.SOUTH else empty, end="")
-        print(wall)
+            cell = maze.grid[i][j]
+            print("|" if cell & Direction.WEST else " ", end="")
+            #print("  ", end="")
+            hex_num = hex(maze.grid[i][j] & 0b00001111)[1:]
+            print(hex_num.replace("x", " "), end="")
+        print()
--- generator.py
+++ generator.py
@@ -1,19 +1,21 @@
 from utils import MazeConfig
-from enum import IntEnum
+from enum import IntFlag, auto
+from random import randrange, choice, seed
+from time import sleep
 
 
-class Direction(IntEnum):
-    NORTH = 0b0001
-    EAST = 0b0010
-    SOUTH = 0b0100
-    WEST = 0b1000
+class Direction(IntFlag):
+    NORTH = auto() # 0001
+    EAST = auto() # 0010
+    SOUTH = auto() # 0100
+    WEST = auto() # 1000
 
 
 class Maze:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        self.grid = [[15] * self.width for _ in range(self.height)]
+        self.grid = [[0] * self.width for _ in range(self.height)]
 
 
 class MazeGenerator:
@@ -36,4 +38,77 @@ class MazeGenerator:
 
     def create(self) -> Maze:
         maze = Maze(self.width, self.height)
+        self.prim(maze.grid)
         return maze
+
+    def prim(self, grid):
+        FRONTIER = 0x20
+        IN = 0x10
+        frontier = []
+        width = self.width
+        height = self.height
+        seed(1234)
+
+        def add_frontier(x, y, grid, frontier):
+            if (x >= 0 and y >= 0 and x < width
+            and y < height and grid[y][x] == 0):
+                grid[y][x] |= FRONTIER
+                frontier.append((x, y))
+
+        def mark(x, y, grid, frontier):
+            grid[y][x] |= IN
+            add_frontier(x - 1, y, grid, frontier)
+            add_frontier(x + 1, y, grid, frontier)
+            add_frontier(x, y - 1, grid, frontier)
+            add_frontier(x, y + 1, grid, frontier)
+
+        def neighbors(x, y, grid):
+            nbs = []
+
+            if x > 0 and grid[y][x - 1] & IN != 0:
+                nbs.append((x - 1, y))
+            if x + 1 < width and grid[y][x + 1] & IN != 0:
+                nbs.append((x + 1, y))
+            if y > 0 and grid[y - 1][x] & IN != 0:
+                nbs.append((x, y - 1))
+            if y + 1 < height and grid[y + 1][x] & IN != 0:
+                nbs.append((x, y + 1))
+
+            return nbs
+
+        def direction(fx, fy, tx, ty):
+            if fx < tx:
+                return Direction.EAST
+            if fx > tx:
+                return Direction.WEST
+            if fy < ty:
+                return Direction.SOUTH
+            if fy > ty:
+                return Direction.NORTH
+
+        def empty(cell):
+            return cell == 0 or cell == FRONTIER
+
+        def get_opposite(dir_: Direction):
+            if dir_ == Direction.NORTH: return Direction.SOUTH
+            if dir_ == Direction.SOUTH: return Direction.NORTH
+            if dir_ == Direction.WEST: return Direction.EAST
+            if dir_ == Direction.EAST: return Direction.WEST
+
+        mark(0, 0, grid, frontier)
+        while frontier:
+            x, y = frontier.pop(randrange(len(frontier)))
+            nb = neighbors(x, y, grid)
+            nx, ny = choice(nb)
+
+            dir_ = direction(x, y, nx, ny)
+            grid[y][x] |= dir_
+            grid[ny][nx] |= get_opposite(dir_)
+
+            mark(x, y, grid, frontier)
+            #m = Maze(0, 0)
+            #m.grid = grid
+            #from draw_maze import draw_maze
+            #print("\033[H", end="")
+            #draw_maze(m)
+            #sleep(0.1)
