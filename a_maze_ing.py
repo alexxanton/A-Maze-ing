6d220292efff33e4e8f2c0b9d63e892cfc8bc5f7
Alex Anton Aranda <aanton-a@cbr1s4.42barcelona.com>
2026-04-10 14:49:57 +0200

    prevented pdb from executing due to incompatibility with curses

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,12 @@
 import sys
+import os
 from src.parser import ConfigParser, ParsingError
 import curses
 from src.interactive_menu import InteractiveMenu
 
 
 def main(stdscr: curses.window) -> None:
+    """Parse the config file and initialize the interactive menu with curses"""
     if len(sys.argv) < 2:
         exit("No config file provided!")
 
@@ -20,6 +22,12 @@ def main(stdscr: curses.window) -> None:
 
 
 if __name__ == "__main__":
+    os.environ["PYTHONBREAKPOINT"] = "0"
+
+    if sys.gettrace() is not None:
+        print("pdb not compatible with curses")
+        sys.exit(1)
+
     try:
         curses.wrapper(main)
     except KeyboardInterrupt:
